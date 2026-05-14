edcef4728d478e81dbe7636c556abd396804860d
sameyboi <channelbeast51@gmail.com>
2026-05-14 13:31:28 +0530

    ok

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  output: "export",
+
   images: {
+    unoptimized: true,
+
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'teotvyqgrbmivctfmval.supabase.co',
+        protocol: "https",
+        hostname: "teotvyqgrbmivctfmval.supabase.co",
       },
     ],
   },
